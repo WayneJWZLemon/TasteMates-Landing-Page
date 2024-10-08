48bbf2d073ba25713e61eecd4c3bd6a3820160d0
Wayne Zheng <wnz6klj@gmail.com>
2024-10-08 01:24:23 -0700

    Metadata update

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-// import { Box, Button, Heading, Text } from "@chakra-ui/react";
-// function App() {
-//   return (
-//     <Box
-//       display="flex"
-//       alignItems="center"
-//       justifyContent="center"
-//       flexDirection="column"
-//       textAlign="center"
-//       py={10}
-//       px={6}
-//     >
-//       <Heading as="h1" size="2xl" mt={6} mb={2}>
-//         Welcome to the exciting world of TasteMates!
-//       </Heading>
-//       <Text color="gray.500" mb={6}>
-//         Stay tuned for our upcoming release.
-//       </Text>
-//       <Button colorScheme="teal" size="lg" disabled>
-//         Get Started
-//       </Button>
-//     </Box>
-//   );
-// }
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+import { Box, Button, Heading, Text } from "@chakra-ui/react";
 function App() {
-    return _jsx("h1", { children: "Hello World!" });
+  return _jsxs(Box, {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    textAlign: "center",
+    py: 10,
+    px: 6,
+    children: [
+      _jsx(Heading, {
+        as: "h1",
+        size: "2xl",
+        mt: 6,
+        mb: 2,
+        children: "Welcome to the exciting world of TasteMates!",
+      }),
+      _jsx(Text, {
+        color: "gray.500",
+        mb: 6,
+        children: "Stay tuned for our upcoming release.",
+      }),
+      _jsx(Button, {
+        colorScheme: "teal",
+        size: "lg",
+        disabled: true,
+        children: "Get Started",
+      }),
+    ],
+  });
 }
 export default App;
--- src/main.js
+++ src/main.js
@@ -3,4 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
-createRoot(document.getElementById("root")).render(_jsx(ChakraProvider, { children: _jsx(App, {}) }));
+createRoot(document.getElementById("root")).render(
+  _jsx(ChakraProvider, { children: _jsx(App, {}) })
+);

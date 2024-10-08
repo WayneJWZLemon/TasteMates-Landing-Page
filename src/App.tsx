3bb23b1fe8b2a6471cc4d9874b526a244ff064dc
Wayne Zheng <wnz6klj@gmail.com>
2024-10-08 01:04:33 -0700

    bring back the right app content

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,27 @@
-// import { Box, Button, Heading, Text } from "@chakra-ui/react";
-
-// function App() {
-//   return (
-//     <Box
-//       display="flex"
-//       alignItems="center"
-//       justifyContent="center"
-//       flexDirection="column"
-//       textAlign="center"
-//       py={10}
-//       px={6}
-//     >
-//       <Heading as="h1" size="2xl" mt={6} mb={2}>
-//         Welcome to the exciting world of TasteMates!
-//       </Heading>
-//       <Text color="gray.500" mb={6}>
-//         Stay tuned for our upcoming release.
-//       </Text>
-//       <Button colorScheme="teal" size="lg" disabled>
-//         Get Started
-//       </Button>
-//     </Box>
-//   );
-// }
+import { Box, Button, Heading, Text } from "@chakra-ui/react";
 
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      flexDirection="column"
+      textAlign="center"
+      py={10}
+      px={6}
+    >
+      <Heading as="h1" size="2xl" mt={6} mb={2}>
+        Welcome to the exciting world of TasteMates!
+      </Heading>
+      <Text color="gray.500" mb={6}>
+        Stay tuned for our upcoming release.
+      </Text>
+      <Button colorScheme="teal" size="lg" disabled>
+        Get Started
+      </Button>
+    </Box>
+  );
 }
 
 export default App;

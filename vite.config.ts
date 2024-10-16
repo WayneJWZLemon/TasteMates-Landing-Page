6457971e6d1caa57366a2932c037d420e8b6f148
Wayne Zheng <wnz6klj@gmail.com>
2024-10-16 11:39:45 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/TasteMates-Landing-Page/",
+  base: "/", // if serving without custom domain with GitHub page then "/TasteMates-Landing-Page/"
 });
